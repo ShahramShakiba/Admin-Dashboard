9c6f0e2ccdf5053b79139984b840406fbc8d993b
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-23 14:22:06 +0330

    Organizing Folders & Files

--- Admin-Dashboard/src/App.jsx
+++ Admin-Dashboard/src/App.jsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 export default function App() {
-  return <></>;
+  return <div className="app"></div>;
 }
--- Admin-Dashboard/src/main.jsx
+++ Admin-Dashboard/src/main.jsx
@@ -1,11 +1,14 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
